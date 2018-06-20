02e1286a62aa0f515d5832a9703a2f5b000f5332
freedomkhanyile <Freedom.Khanyile1@gmail.com>
2018-06-20 19:43:20 +0200

    Student - Update password

--- src/app/lecture/lecture-change-password/lecture-change-password.component.ts
+++ src/app/lecture/lecture-change-password/lecture-change-password.component.ts
@@ -1,5 +1,5 @@
 import { User } from './../../shared/user';
- 
+
 import { Component, OnInit } from '@angular/core';
 import { LectureService } from '../../admin/lecture/lecture.service';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -12,92 +12,88 @@ import { UserDataService } from '../../shared/services/user-data.service';
   styleUrls: ['./lecture-change-password.component.css']
 })
 export class LectureChangePasswordComponent implements OnInit {
-  public confirmpassword : string;
-  public oldpassword : string;
-  public newpassword : string;
-  public lecture : any;
+  public confirmpassword: string;
+  public oldpassword: string;
+  public newpassword: string;
+  public lecture: any;
   public user: User;
   public message: any;
   constructor(
-    private lectureService: LectureService
-    ,private router: Router
-    ,private route: ActivatedRoute
-    ,private selectService: SelectService
-    ,private userService: UserDataService
-      ) { }
+    private router: Router
+    , private userService: UserDataService
+  ) { }
 
-      ngOnInit() {
-        this.assignValues();
-      } 
-      assignValues(){
-        this.lecture = this.userService.getUser();
-        if(this.lecture){
-          this.user = new User();
-          this.user.id = this.lecture.id;
-          this.user.name = this.lecture.name;
-          this.user.surname = this.lecture.surname;
-          this.user.cell = this.lecture.cell;
-          this.user.city = this.lecture.city;
-          this.user.address = this.lecture.address;
-          this.user.idnumber = this.lecture.idnumber;
-          this.user.email = this.lecture.email;
-          this.user.password = this.lecture.password;
-        }
-        else{
-          this.router.navigate(['/lecture-dashboard']);
-        }
+  ngOnInit() {
+    this.assignValues();
+  }
+  assignValues() {
+    this.lecture = this.userService.getUser();
+    if (this.lecture) {
+      this.user = new User();
+      this.user.id = this.lecture.id;
+      this.user.name = this.lecture.name;
+      this.user.surname = this.lecture.surname;
+      this.user.cell = this.lecture.cell;
+      this.user.city = this.lecture.city;
+      this.user.address = this.lecture.address;
+      this.user.idnumber = this.lecture.idnumber;
+      this.user.email = this.lecture.email;
+      this.user.password = this.lecture.password;
+    }
+    else {
+      this.router.navigate(['/lecture-dashboard']);
+    }
+  }
+  Abort() {
+    this.userService.saveUser(this.lecture);
+    this.router.navigate(['/lecture-dashboard']);
+  }
+  isValid: Boolean;
+  Save() {
+    this.isValid = true;
+    debugger
+    if (this.oldpassword == this.user.password) {
+      if (this.newpassword == null || this.newpassword == "" ||
+        this.confirmpassword == "" || this.confirmpassword == null) {
+        this.isValid = false;
+        this.message = "fields to be updated cannot be empty";
+        return;
       }
-      Abort(){
-        this.userService.saveUser(this.lecture);
-        this.router.navigate(['/lecture-dashboard']);
+      if (this.newpassword.length < 6) {
+        this.isValid = false;
+        this.message = "Password lenght can not be less than 6 characters";
+        this.newpassword = null;
+        this.oldpassword = null;
+        this.confirmpassword = null;
+        return;
       }
-      isValid : Boolean;
-      Save(){
-        this.isValid = true;
-          debugger
-        if (this.oldpassword == this.user.password) {
-            if ( this.newpassword == null || this.newpassword == "" || 
-            this.confirmpassword == "" || this.confirmpassword == null )
-            {
-              this.isValid = false;
-              this.message = "fields to be updated cannot be empty";
-              return;
-            }
-            if(this.newpassword.length < 6){
-              this.isValid = false;
-              this.message = "Password lenght can not be less than 6 characters";
-              this.newpassword = null;
-              this.oldpassword = null;
-              this.confirmpassword = null;
-              return;
-            }
-            if(this.newpassword != this.confirmpassword){
-              this.isValid = false;
-              this.message = "Passwords do not Match, please check and continue";
-              this.newpassword = null;
-              this.oldpassword = null;
-              this.confirmpassword = null;
-              return;
-            }
-            if(this.isValid){
-              var data = {
-                password: this.newpassword,
-                email : this.user.email
-              }
-              this.userService.updatePassword(data).subscribe(response => {
-                if(response == 1){
-                  alert("Account Details Updated Successfully - Click Ok to proceed");
-                  this.user.password = this.newpassword;
-                  this.userService.saveUser(this.user);
-                  this.router.navigate(['/lecture-dashboard']);
-                }
-                else{
-                  this.message = response;
-                }
-              })
-            }
-        } else {
-          this.message = "Please enter correct Credentials and Continue";
+      if (this.newpassword != this.confirmpassword) {
+        this.isValid = false;
+        this.message = "Passwords do not Match, please check and continue";
+        this.newpassword = null;
+        this.oldpassword = null;
+        this.confirmpassword = null;
+        return;
+      }
+      if (this.isValid) {
+        var data = {
+          password: this.newpassword,
+          email: this.user.email
         }
+        this.userService.updatePassword(data).subscribe(response => {
+          if (response == 1) {
+            alert("Account Details Updated Successfully - Click Ok to proceed");
+            this.user.password = this.newpassword;
+            this.userService.saveUser(this.user);
+            this.router.navigate(['/lecture-dashboard']);
+          }
+          else {
+            this.message = response;
+          }
+        })
       }
+    } else {
+      this.message = "Please enter correct Credentials and Continue";
+    }
+  }
 }
--- src/app/student/student-change-password/student-change-password.component.ts
+++ src/app/student/student-change-password/student-change-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../shared/user';
+import { UserDataService } from '../../shared/services/user-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-student-change-password',
@@ -7,9 +10,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  public confirmpassword: string;
+  public oldpassword: string;
+  public newpassword: string;
+  public student: any;
+  public user: User;
+  public message: any;
+  constructor(
+    private router: Router
+    , private userService: UserDataService
+  ) { }
 
   ngOnInit() {
+    this.assignValues();
+  }
+  assignValues() {
+    this.student = this.userService.getUser();
+    if (this.student) {
+      this.user = new User();
+      this.user.id = this.student.id;
+      this.user.name = this.student.name;
+      this.user.surname = this.student.surname;
+      this.user.cell = this.student.cell;
+      this.user.city = this.student.city;
+      this.user.address = this.student.address;
+      this.user.idnumber = this.student.idnumber;
+      this.user.email = this.student.email;
+      this.user.password = this.student.password;
+    }
+    else {
+      this.router.navigate(['/student-dashboard']);
+    }
+  }
+  Abort() {
+    this.userService.saveUser(this.student);
+    this.router.navigate(['/student-dashboard']);
+  }
+  isValid: Boolean;
+  Save() {
+    this.isValid = true;
+    debugger
+    if (this.oldpassword == this.user.password) {
+      if (this.newpassword == null || this.newpassword == "" ||
+        this.confirmpassword == "" || this.confirmpassword == null) {
+        this.isValid = false;
+        this.message = "fields to be updated cannot be empty";
+        return;
+      }
+      if (this.newpassword.length < 6) {
+        this.isValid = false;
+        this.message = "Password lenght can not be less than 6 characters";
+        this.newpassword = null;
+        this.oldpassword = null;
+        this.confirmpassword = null;
+        return;
+      }
+      if (this.newpassword != this.confirmpassword) {
+        this.isValid = false;
+        this.message = "Passwords do not Match, please check and continue";
+        this.newpassword = null;
+        this.oldpassword = null;
+        this.confirmpassword = null;
+        return;
+      }
+      if (this.isValid) {
+        var data = {
+          password: this.newpassword,
+          email: this.user.email
+        }
+        this.userService.updatePassword(data).subscribe(response => {
+          if (response == 1) {
+            alert("Account Details Updated Successfully - Click Ok to proceed");
+            this.user.password = this.newpassword;
+            this.userService.saveUser(this.user);
+            this.router.navigate(['/student-dashboard']);
+          }
+          else {
+            this.message = response;
+          }
+        })
+      }
+    } else {
+      this.message = "Please enter correct Credentials and Continue";
+    }
   }
 
 }
