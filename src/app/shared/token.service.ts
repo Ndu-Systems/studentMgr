8aeb31b01b2a1342f05bf3a99776d9ef0d3ccb1b
freedomkhanyile <Freedom.Khanyile1@gmail.com>
2018-06-23 10:08:50 +0200

    Merge and DB Changes

--- src/app/shared/token.service.ts
+++ src/app/shared/token.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_URL } from './config';
 
 @Injectable()
 export class TokenService {
 
-constructor() { }
-
+    url: string = API_URL;
+    constructor(private http:HttpClient) { }
+     
 }
